5632453217ecd69bfbe07d568930bf93c1a6293a
Nenu Adrian <adrian.nenu@gmail.com>
2022-08-06 11:38:13 -0400

    Update constants.php

--- includes/constants/constants.php
+++ includes/constants/constants.php
@@ -2,6 +2,8 @@
 /* Nenu Adrian Mircea 2012+ */
 	return [
 		'url' => $_SERVER['REQUEST_SCHEME'] .  '://' . $_SERVER['HTTP_HOST'] . str_replace('index.php', '', $_SERVER['SCRIPT_NAME']),
+		'contact_email' => 'undefined@undefined.com',
+		
 		'tutorialSteps' => 20,
 
 		'gridBrowseZone' => 5,
@@ -23,7 +25,7 @@
 		"smtp_username" => "",
 		"smtp_password" => "",
 		"smtp_name" => "Secret Republic",
-		"smtp_from" =>"do-not-reply@secretrepublic-v3.nenuadrian.com",
+		"smtp_from" =>"undefined@undefined.com",
 		"smtp_secure" => "tls",
 		"smtp_port" =>587,
 
