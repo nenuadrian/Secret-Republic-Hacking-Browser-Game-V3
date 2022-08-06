444dd5a54b9591637043f63326cac9c4358568c0
Nenu Adrian <adrian.nenu@gmail.com>
2022-08-06 12:03:22 -0400

    Update attacksManagement.php

--- includes/modules/admin/attacksManagement.php
+++ includes/modules/admin/attacksManagement.php
@@ -2,9 +2,9 @@
 
 if ($GET['logs'])
 {
-  $db->join("users sender", "sender.id = a.sender_id", "left outer");
-  $db->join("users receiver", "receiver.id = a.receiver_id", "left outer");
-  if ($GET['hacker']) $db->where('a.sender_id = ? or a.receiver_id = ?', array($GET['hacker'], $GET['hacker']));
+  $db->join("users sender", "sender.id = a.sender_user_id", "left outer");
+  $db->join("users receiver", "receiver.id = a.receiver_user_id", "left outer");
+  if ($GET['hacker']) $db->where('a.sender_user_id = ? or a.receiver_user_id = ?', array($GET['hacker'], $GET['hacker']));
 	
   $attacks = $db->get('attack_logs a', null, "a.*, sender.username sender_username, receiver.username receiver_username");
 
@@ -16,9 +16,9 @@ if ($GET['logs'])
 else
 {
   
-  $db->join("users sender", "sender.id = a.sender_id", "left outer");
-  $db->join("users receiver", "receiver.id = a.receiver_id", "left outer");
-  if ($GET['hacker']) $db->where('a.sender_id = ? or a.receiver_id = ?', array($GET['hacker'], $GET['hacker']));
+  $db->join("users sender", "sender.id = a.sender_user_id", "left outer");
+  $db->join("users receiver", "receiver.id = a.receiver_user_id", "left outer");
+  if ($GET['hacker']) $db->where('a.sender_user_id = ? or a.receiver_user_id = ?', array($GET['hacker'], $GET['hacker']));
 	
   $attacks = $db->get('attacks_inprogress a', null, "a.*, sender.username sender_username, receiver.username receiver_username");
 
