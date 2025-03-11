280b219affde6958fbb5b66b42cbf401d24c02ba
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2025-03-11 19:12:10 -0400

    minor fixes

--- public_html/api.php
+++ public_html/api.php
@@ -1,10 +1,7 @@
 <?php
 
-if ($_POST["time"])
-{
+if ($_POST["time"]) {
   date_default_timezone_set("Europe/London");
-
   echo json_encode(array("time" => date("H:i", time())));
-} elseif ($_GET['mobile_server_check']) 
-	echo "true";
-else header("Location: index.php");
+} else
+  header("Location: index.php");
